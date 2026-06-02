31e3e92ce7f596f7b8ceb493d2f56af8538b74d6
Lonewolfrex <samparkprakash27@gmail.com>
2026-06-02 19:22:01 +0530

    Cosmetic changes pushed

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,39 +4,29 @@ from rest_framework import status
 
 from .serializers import RegisterSerializer
 from rest_framework.permissions import IsAuthenticated
+from chat.models import ChatSession
 
 class RegisterView(APIView):
 
     def post(self, request):
-
-        serializer = RegisterSerializer(
-            data=request.data
-        )
-
+        serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
 
-            serializer.save()
+            user = serializer.save()
+
+            ChatSession.objects.create(
+                user=user,
+                title="Welcome Chat"
+            )
 
             return Response(
-                {
-                    "message": "User created"
-                },
+                {"message": "User created"},
                 status=201
             )
 
-        return Response(
-            serializer.errors,
-            status=400
-        )
-
 class MeView(APIView):
-
-    permission_classes = [
-        IsAuthenticated
-    ]
-
+    permission_classes = [IsAuthenticated]
     def get(self, request):
-
         return Response({
             "username": request.user.username,
             "email": request.user.email,
--- backend/frontend_ui/views.py
+++ backend/frontend_ui/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 
+
+@never_cache
 def login_page(request):
-    return render(request, "login.html")
+    response = render(request, "login.html")
+    response["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response["Pragma"] = "no-cache"
+    response["Expires"] = "0"
+    return response
 
+
+@never_cache
 def chat_page(request):
-    return render(request, "chat.html")
+    response = render(request, "chat.html")
+    response["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response["Pragma"] = "no-cache"
+    response["Expires"] = "0"
+    return response
+
+@never_cache
+def register_page(request):
+    return render(request,"register.html")
